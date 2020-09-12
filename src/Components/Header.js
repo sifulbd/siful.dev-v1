7cfb35d43eefe788e36786cf2c54639fa72ec673
Siful Islam <sifulsmm@gmail.com>
2020-09-12 13:11:32 +0600

    nav update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -23,9 +23,9 @@ class Header extends Component {
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
+	         <li><a className="smoothscroll" href="#resume">Skills</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            {/* <li><a className="smoothscroll" href="#contact">Contact</a></li> */}
          </ul>
       </nav>
 
